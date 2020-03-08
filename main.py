d8d5bcb60a0ea37247c159f16a3a584dabe9ab1a
Dimitrios Synapalos <dsynapalos@algosystems.gr>
2020-03-08 18:43:16 +0200

    .1

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import datetime
 import hashlib
 import json
-from flask import Flask,jsonify
+from flask import Flask, jsonify
+
+
+class Blockchain():
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash=0)
+
+    def create_block(self, proof, previous_hash):
+        return True
--- testing/test_block.py
+++ testing/test_block.py
@@ -1,2 +1,19 @@
-def test_pytest_true():
+def test_pytest_is_working():
     assert True
+
+
+def test_class_Blockchain_is_defined():
+    from main import Blockchain
+    assert Blockchain()
+
+
+def test_class_Blockchain_method_chain_is_empty_list():
+    from main import Blockchain
+    x = Blockchain()
+    assert x.chain == []
+
+
+def test_class_Blockchain_method_create_block_is_defined():
+    from main import Blockchain
+    x = Blockchain()
+    assert x.create_block(proof=1, previous_hash=0)
